6f8a5e544cc20dfdb5bedddd4f91551f72680bb3
cernoh <zoiny@outlook.com>
2024-12-04 14:02:42 +0000

    part 2

--- day2/main.cpp
+++ day2/main.cpp
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <cstdio>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -23,8 +24,7 @@ std::vector<std::vector<int>> readFileToVector(const std::string &filename) {
   return result;
 }
 
-int main() {
-  std::string filename = "input.txt";
+int part1(std::string &filename) {
   std::vector<std::vector<int>> data = readFileToVector(filename);
 
   int total = 0;
@@ -50,6 +50,47 @@ int main() {
     }
   }
 
-  std::cout << "Total readable: " << total << std::endl;
+  return total;
+}
+
+int part2(std::string &filename) {
+  std::vector<std::vector<int>> data = readFileToVector(filename);
+
+  int total = 0;
+
+  for (const auto &row : data) {
+    bool readable = true;
+    bool safety = true;
+    bool incrementing = row[0] < row[1];
+    int prev_number = row[0];
+
+    for (size_t i = 1; i < row.size(); ++i) {
+      int number = row[i];
+      if ((incrementing && number <= prev_number) ||
+          (!incrementing && number >= prev_number) ||
+          abs(number - prev_number) > 3) {
+        if (!safety) {
+          readable = false;
+          break;
+        } else {
+          safety = false;
+        }
+      }
+      prev_number = number;
+    }
+
+    if (readable) {
+      total++;
+    }
+  }
+
+  return total;
+}
+
+int main() {
+  std::string filename = "input.txt";
+  int total = part1(filename);
+  int total2 = part2(filename);
+  std::cout << "part 1:" << total << "\n part 2: " << total2 << std::endl;
   return 0;
 }
